2df00f56eea488cfd23ed8cef653a596b47481f3
Ali KADAM <Ali KADAM>
2017-05-02 14:20:15 +0300

    phpresponse

--- index.php
+++ index.php
@@ -23,30 +23,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 	echo $bilgiler[1];
 	echo $bilgiler[2];
 	
-	$sehirler=explode("->", $bilgiler[1]);
-	echo $sehirler[0]; // dilim1
-	echo $sehirler[1];
 	
-	$params = array ('apikey'=>'it393887540691813618334846166341');
-	
-	// Build Http query using params
-	$query = http_build_query ($params);
-	
-	// Create Http context details
-	$contextData = array (
-			'method' => 'GET',
-			'header' => "Connection: close\r\n".
-			"Content-Length: ".strlen($query)."\r\n",
-			'content'=> $query );
-	
-	// Create context resource for our request
-	$context = stream_context_create (array ( 'http' => $contextData ));
-	
-	// Read page rendered as result of your POST request
-	$result =  file_get_contents (
-			'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/UK/GBP/en-GB/'.$sehirler[0].'-sky/'.$sehirler[1].'-sky/'.$bilgiler[0].'/',  // page url
-			false,
-			$context);
 	
 	$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAZAkqHoon8EBAP7XJjyjucsUEuhkkE7CMxZCDjAMje30nZAjgcZBltyIkUEK3SG1ZCYJPikVN874gbZCrhS9y6P7hrAoBwOZC3mMBtlpncoL9WLZC3BzdtmmMC4I8amPTRTIOPl1yw1O9avB63shH5oMHCPpMtVXps1DZBNiyGtmiwZDZD';
 	
@@ -58,7 +35,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
         "id":"' . $sender . '"
         },
         "message":{
-            "text": "'. $bilgiler[0] .'  Tarihinde ' . $bilgiler[1]. ' istikametinde ' . $bilgiler[2]. ' kisilik yer '.$result.'"
+            "text": "'. $bilgiler[0] .'  Tarihinde ' . $bilgiler[1]. ' istikametinde ' . $bilgiler[2]. ' kisilik yer " 
         }
     }';
 	/* curl setting to send a json post data */
--- messagebot.php
+++ messagebot.php
@@ -18,6 +18,31 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 	$sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
 	$message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
 	
+	$sehirler=explode("->", $bilgiler[1]);
+	echo $sehirler[0]; // dilim1
+	echo $sehirler[1];
+	
+	$params = array ('apikey'=>'it393887540691813618334846166341');
+	
+	// Build Http query using params
+	$query = http_build_query ($params);
+	
+	// Create Http context details
+	$contextData = array (
+			'method' => 'GET',
+			'header' => "Connection: close\r\n".
+			"Content-Length: ".strlen($query)."\r\n",
+			'content'=> $query );
+	
+	// Create context resource for our request
+	$context = stream_context_create (array ( 'http' => $contextData ));
+	
+	// Read page rendered as result of your POST request
+	$result =  file_get_contents (
+			'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/UK/GBP/en-GB/'.$sehirler[0].'-sky/'.$sehirler[1].'-sky/'.$bilgiler[0].'/',  // page url
+			false,
+			$context);
+	
 	$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_ACCESS_TOKEN';
 	
 	/*initialize curl*/
