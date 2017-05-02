8e3a452809e0b7070bcaac1e0e5d18876548620d
Ali KADAM <Ali KADAM>
2017-05-02 20:50:11 +0300

    elma

--- index.php
+++ index.php
@@ -27,9 +27,26 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 	//echo $sehirler[0]; // dilim1
 	//echo $sehirler[1];
 	
-	$urlpath="http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/UK/GBP/en-GB/'.$sehirler[0].'-sky/'.$sehirler[1].'-sky/'.$bilgiler[0].'/?apikey=it393887540691813618334846166341";
+	$getdata = http_build_query(
+			array(
+					'apikey'=>'it393887540691813618334846166341'					
+			)
+			);
 	
-	$xml = file_get_contents($urlpath);
+	$opts = array('http' =>
+			array(
+					'method'  => 'GET',
+					'content' => $getdata
+			)
+	);
+	
+	$context  = stream_context_create($opts);
+	
+	$result = file_get_contents("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/UK/GBP/en-GB/'.$sehirler[0].'-sky/'.$sehirler[1].'-sky/'.$bilgiler[0].'/", false, $context);
+	
+	//$urlpath=
+	
+	//$xml = file_get_contents($urlpath);
 	
 	$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAZAkqHoon8EBAP7XJjyjucsUEuhkkE7CMxZCDjAMje30nZAjgcZBltyIkUEK3SG1ZCYJPikVN874gbZCrhS9y6P7hrAoBwOZC3mMBtlpncoL9WLZC3BzdtmmMC4I8amPTRTIOPl1yw1O9avB63shH5oMHCPpMtVXps1DZBNiyGtmiwZDZD';
 	
@@ -41,7 +58,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
         "id":"' . $sender . '"
         },
         "message":{
-            "text": " '.$bilgiler[0].'   Tarihinde '.$bilgiler[1].' istikametinde  '.$bilgiler[2].' kisilik yer '.$xml.' lar mevcut " 
+            "text": " '.$bilgiler[0].'   Tarihinde '.$bilgiler[1].' istikametinde  '.$bilgiler[2].' kisilik yer '.$result.' lar mevcut " 
         }
     }';
 	/* curl setting to send a json post data */
